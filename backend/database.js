33799ebc00b5d457d4bdbbb2ae4f00782d074b87
Aditya Kumar Jha <aditya.ibm.kumar@gmail.com>
2025-04-28 21:57:21 +0530

    login-singup-mongodb

--- backend/database.js
+++ backend/database.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose');
 async function dbConn(){
     try{
-        await mongoose.connect("mongodb://localhost:27017/userlogin");
+        await mongoose.connect("mongodb+srv://adityajha:root@cluster0.slgay.mongodb.net/Login-Singup-mongodb?retryWrites=true&w=majority&appName=Cluster0");
         console.log("Database connection done successfully!!! ")
     }catch(err){
        console.log(err.message)
